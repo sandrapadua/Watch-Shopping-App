b60e97c4a4f101aece0c7acff40b8377788cc3f9
Sandra Padua <sandrapadua5@gmail.com>
2019-06-13 12:49:39 +0200

    aded initial state to reducers

---
+++ reducers.js
@@ -0,0 +1,32 @@
+const initialState = [
+{
+    id:1,
+    Manufacturer: 'Fastrack', 
+    desc: "Water Resistant, AnalogType, Style Code NF3123SL02C, Wrist Watch.",
+    Material:'Leather',
+    color:'Brown',
+    Warranty: 'one year',
+    Price:$100
+},
+{
+    id:2,
+    Manufacturer: 'Casio', 
+    desc: "Water Resistant, AnalogType, Style Code NF3123SL02C, Wrist Watch.",
+    Material:'stainless stee;',
+    color:'Silver',
+    Warranty: 'one year',
+    Price:$200
+},
+{
+    id:3, 
+    Manufacturer: 'Fogg', 
+    desc: "Water Resistant, AnalogType, Style Code NF3123SL02C, Wrist Watch.",
+    Material:'Leather',
+    color:'Brown',
+    Warranty: 'one year',
+    Price:$80
+}
+
+
+
+]
